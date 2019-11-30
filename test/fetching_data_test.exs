b4d1026548e39929827f88d4ecdf5125828277bb
Andrew Callahan <andrew.callahan@gmail.com>
2019-11-30 16:56:44 -0500

    add more tests

--- test/fetching_data_test.exs
+++ test/fetching_data_test.exs
@@ -15,6 +15,7 @@ defmodule AshJsonApiTest.FetchingData do
 
     # describe "200 OK" do
     test "A server MUST respond to a successful request to fetch an individual resource with a 200 OK response." do
+      # Create a post
       {:ok, post} = Ash.create(AshJsonApi.Test.Resources.Post, %{attributes: %{name: "foo"}})
 
       # Create a test connection
@@ -40,13 +41,84 @@ defmodule AshJsonApiTest.FetchingData do
       assert conn.status == 200
     end
 
-    # test "A server MUST respond to a successful request to fetch a resource collection with an array of resource objects or an empty array ([]) as the response document’s primary data." do
+    test "A server MUST respond to a successful request to fetch a resource collection with an empty array ([]) as the response document’s primary data when there is no data." do
+      # Create a test connection
+      conn = conn(:get, "/posts")
 
-    # end
+      # Invoke the plug
+      conn = AshJsonApi.Test.Router.call(conn, @router_opts)
 
-    # test "A server MUST respond to a successful request to fetch an individual resource with a resource object or null provided as the response document’s primary data." do
-    #   # confused as to why this is a 200 and not 404
-    # end
+      # Assert the response state
+      assert conn.state == :sent
+
+      # Assert the data attribute of the response body
+      assert Jason.decode!(conn.resp_body)["data"] == []
+    end
+
+    test "A server MUST respond to a successful request to fetch a resource collection with an array of resource objects as the response document’s primary data when there is data." do
+      # Create a post
+      {:ok, post} = Ash.create(AshJsonApi.Test.Resources.Post, %{attributes: %{name: "foo"}})
+
+      # Create a test connection
+      conn = conn(:get, "/posts")
+
+      # Invoke the plug
+      conn = AshJsonApi.Test.Router.call(conn, @router_opts)
+
+      # Assert the response state
+      assert conn.state == :sent
+
+      # Assert the data attribute of the response body
+      assert Jason.decode!(conn.resp_body)["data"] == [
+               %{
+                 "attributes" => %{"name" => post.name},
+                 "id" => post.id,
+                 "links" => %{},
+                 "relationships" => %{},
+                 "type" => "post"
+               }
+             ]
+    end
+
+    test "A server MUST respond to a successful request to fetch an individual resource with a resource object provided as the response document’s primary data when there is data." do
+      # Create a post
+      {:ok, post} = Ash.create(AshJsonApi.Test.Resources.Post, %{attributes: %{name: "foo"}})
+
+      # Create a test connection
+      conn = conn(:get, "/posts/#{post.id}")
+
+      # Invoke the plug
+      conn = AshJsonApi.Test.Router.call(conn, @router_opts)
+
+      # Assert the response state
+      assert conn.state == :sent
+
+      # Assert the data attribute of the response body
+      assert Jason.decode!(conn.resp_body)["data"] == %{
+               "attributes" => %{"name" => post.name},
+               "id" => post.id,
+               "links" => %{},
+               "relationships" => %{},
+               "type" => "post"
+             }
+    end
+
+    test "A server MUST respond to a successful request to fetch an individual resource with null provided as the response document’s primary data when there is no data" do
+      # Create a test connection
+      conn = conn(:get, "/posts/#{Ash.UUID.generate}")
+
+      # Invoke the plug
+      conn = AshJsonApi.Test.Router.call(conn, @router_opts)
+
+      # Assert the response state
+      assert conn.state == :sent
+
+      # Assert the data attribute of the response body
+      # TODO: is testing for nil the same as testing for null?
+      # TODO: errors are printing to the terminal screen when this test runs - noise we could do without for passing tests
+      assert Jason.decode!(conn.resp_body)["data"] == nil
+    end
+
     # end
 
     # describe "404 Not Found" do
