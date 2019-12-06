627a73da086eefb8f274fe8fec2e227ddef5ed23
Andrew Callahan <andrew.callahan@gmail.com>
2019-12-05 23:08:56 -0500

    redo test names for content negotiation

--- test/spec_compliance/content_negotiation_test.exs
+++ test/spec_compliance/content_negotiation_test.exs
@@ -65,22 +65,38 @@ defmodule AshJsonApi.ContentNegotiationTest do
   end
 
   @tag :spec_must
-  describe "Clients MUST send all JSON:API data in request documents with the header Content-Type: application/vnd.api+json without any media type parameters." do
+  # JSON:API 1.0 Specification
+  # --------------------------
+  # Clients MUST send all JSON:API data in request documents with the header Content-Type: application/vnd.api+json without any media type parameters.
+  # --------------------------
+  describe "Client sending request Content-Type header" do
     # N/A
   end
 
   @tag :spec_must
-  describe "Clients that include the JSON:API media type in their Accept header MUST specify the media type there at least once without any media type parameters." do
+  # JSON:API 1.0 Specification
+  # --------------------------
+  # Clients that include the JSON:API media type in their Accept header MUST specify the media type there at least once without any media type parameters.
+  # --------------------------
+  describe "Client sending request Accept header" do
     # N/A
   end
 
   @tag :spec_must
-  describe "Clients MUST ignore any parameters for the application/vnd.api+json media type received in the Content-Type header of response documents." do
+  # JSON:API 1.0 Specification
+  # --------------------------
+  # Clients MUST ignore any parameters for the application/vnd.api+json media type received in the Content-Type header of response documents.
+  # --------------------------
+  describe "Client processing response Content-Type header" do
     # N/A
   end
 
   @tag :spec_must
-  describe "Servers MUST send all JSON:API data in response documents with the header Content-Type: application/vnd.api+json without any media type parameters." do
+  # JSON:API 1.0 Specification
+  # --------------------------
+  # Servers MUST send all JSON:API data in response documents with the header Content-Type: application/vnd.api+json without any media type parameters.
+  # --------------------------
+  describe "Server sending Content-Type header in response" do
     # TODO: This test should run as part of ALL responses - not just this one off example below, similar to JSON Schema validations
     test "individual resource" do
       # Create a post
@@ -94,7 +110,11 @@ defmodule AshJsonApi.ContentNegotiationTest do
   end
 
   @tag :spec_must
-  describe "Servers MUST respond with a 415 Unsupported Media Type status code if a request specifies the header Content-Type: application/vnd.api+json with any media type parameters." do
+  # JSON:API 1.0 Specification
+  # --------------------------
+  # Servers MUST respond with a 415 Unsupported Media Type status code if a request specifies the header Content-Type: application/vnd.api+json with any media type parameters.
+  # --------------------------
+  describe "Server sending 415 Unsupported Media Type" do
     test "request Content-Type header is not present" do
       # Create a post
       {:ok, post} = Api.create(Post, %{attributes: %{name: "foo"}})
@@ -192,7 +212,11 @@ defmodule AshJsonApi.ContentNegotiationTest do
   end
 
   @tag :spec_must
-  describe "Servers MUST respond with a 406 Not Acceptable status code if a request’s Accept header contains the JSON:API media type and all instances of that media type are modified with media type parameters." do
+  # JSON:API 1.0 Specification
+  # --------------------------
+  # Servers MUST respond with a 406 Not Acceptable status code if a request’s Accept header contains the JSON:API media type and all instances of that media type are modified with media type parameters.
+  # --------------------------
+  describe "Server sending 406 Not Acceptable" do
     test "request Accept header is not present" do
       # Create a post
       {:ok, post} = Api.create(Post, %{attributes: %{name: "foo"}})
