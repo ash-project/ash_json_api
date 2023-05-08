96552b45caf4fd444db1ca7f8593138f242728f2
Skander Mzali <skanderm@gmail.com>
2023-05-08 15:02:22 -0700

    Remove id from open_api as well

--- lib/ash_json_api/json_schema/open_api.ex
+++ lib/ash_json_api/json_schema/open_api.ex
@@ -200,6 +200,7 @@ if Code.ensure_loaded?(OpenApiSpex) do
     defp resource_attributes(resource) do
       resource
       |> Ash.Resource.Info.public_attributes()
+      |> Enum.reject(& &1.name == :id)
       |> Map.new(fn attr ->
         {attr.name, resource_attribute_type(attr)}
       end)
@@ -264,7 +265,7 @@ if Code.ensure_loaded?(OpenApiSpex) do
     defp required_attributes(resource) do
       resource
       |> Ash.Resource.Info.public_attributes()
-      |> Enum.reject(& &1.allow_nil?)
+      |> Enum.reject(& &1.allow_nil? || &1.name == :id)
       |> Enum.map(& &1.name)
       |> case do
         [] -> nil
