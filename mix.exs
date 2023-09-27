086f73e52cf89b5fb5b9260215cdd1c4bfd6cb99
Dmitry Maganov <vonagam@gmail.com>
2023-09-27 08:23:00 +0300

    fix: remove `AshJsonApi.Application` (#99)

--- mix.exs
+++ mix.exs
@@ -139,8 +139,7 @@ defmodule AshJsonApi.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
-      mod: {AshJsonApi.Application, []}
+      extra_applications: [:logger]
     ]
   end
 
