c8df42bce9af182c01111a9f1d8d990b37e64672
Minsub Kim <rapidfsub@gmail.com>
2024-07-30 17:50:10 +0900

    fix: reject allow_nil_input fields in required_write_attributes

--- lib/ash_json_api/json_schema/json_schema.ex
+++ lib/ash_json_api/json_schema/json_schema.ex
@@ -927,7 +927,10 @@ defmodule AshJsonApi.JsonSchema do
         resource
         |> Ash.Resource.Info.attributes()
         |> Enum.filter(&(&1.name in action.accept && &1.writable?))
-        |> Enum.reject(&(&1.allow_nil? || not is_nil(&1.default) || &1.generated?))
+        |> Enum.reject(
+          &(&1.allow_nil? || not is_nil(&1.default) || &1.generated? ||
+              &1.name in action.allow_nil_input)
+        )
         |> Enum.map(&to_string(&1.name))
       end
 
