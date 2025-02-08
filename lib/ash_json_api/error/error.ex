83f74bac8916eb8c9dd6e9922360924012779e0c
Maciej Malecki <maciej@smefju.pl>
2025-02-08 13:36:26 +0100

    Include source pointer in the debug logs
    Otherwise, log message will look like:
    
        [error] required: Required | is required

--- lib/ash_json_api/error/error.ex
+++ lib/ash_json_api/error/error.ex
@@ -160,7 +160,14 @@ defmodule AshJsonApi.Error do
           "No description"
       end
 
-    [code, title, " | ", description]
+    source_pointer =
+      if is_bitstring(error.source_pointer) do
+        error.source_pointer
+      else
+        "|"
+      end
+
+    [code, title, " ", source_pointer, " ", description]
   end
 
   def with_source_pointer(%{source_pointer: source_pointer} = built_error, _, _, _)
