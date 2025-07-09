2a442e2250f5005ba99c180a404f1981d095d466
Cyprien Poisson <c.poisson@cpi-services.fr>
2025-07-09 14:30:40 +0200

    nested record duplicates

--- lib/ash_json_api/includes/includer.ex
+++ lib/ash_json_api/includes/includer.ex
@@ -45,6 +45,7 @@ defmodule AshJsonApi.Includes.Includer do
             |> Map.get(relationship, [])
             |> List.wrap()
           end)
+          |> Enum.uniq()
           |> get_includes_map(further, includes_map)
 
         preloaded_with_linkage =
