6ab6b12b3ec9600d4603eec3bfa78b1e2cd31531
Skander Mzali <skanderm@gmail.com>
2025-09-16 09:37:05 -0700

    chore: bump ash minimum version to 3.5.39

--- mix.exs
+++ mix.exs
@@ -130,7 +130,7 @@ defmodule AshJsonApi.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ash, ash_version("~> 3.0 and >= 3.4.69")},
+      {:ash, ash_version("~> 3.0 and >= 3.5.39")},
       {:spark, "~> 2.2 and >= 2.2.10"},
       {:igniter, "~> 0.3 and >= 0.3.58", optional: true},
       {:plug, "~> 1.11"},
